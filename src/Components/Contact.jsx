585a4a3493e3c77817b6efab149a1d0ae61c2337
Shivangi020 <me.shivasingh.0987@gmail.com>
2023-09-07 20:45:17 +0530

    Building :contact form

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import Heading from './Heading'
+import React from "react";
+import Heading from "./Heading";
 
 const Contact = () => {
   return (
-    <div className='contact-cn' id='#Contact'>
-         <Heading heading={'Connect Today'} comment={"// Let's Build Web Together"}/>
-         <section></section>
+    <div className="contact-cn" id="Contact">
+      <Heading
+        heading={"Connect Today"}
+        comment={"// Let's Build Web Together"}
+      />
+      <section className="contact-s">
+        <form className="contact-form">
+          <input type="text" name="from_name" placeholder="Name"/>
+          <input type="email" name="from_email" placeholder="Your Email" />
+          <input type="text" name="subject" placeholder="Subject" />
+          <textarea name="html_message" placeholder="Message"></textarea>
+          <input type="submit" value="Send" />
+        </form>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
